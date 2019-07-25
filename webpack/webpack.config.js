780000cbef2155146a66ba4231fbfe316746c33c
ShaoBing7 <1556594881@qq.com>
2019-07-25 16:25:50 +0800

    44

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,53 +1,76 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const glob              = require("glob")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-module.exports = {
+const PurifyCSSPlugin   = require('purifycss-webpack');
+      module.exports    = {
   entry:{
-    index:"./src/index.js"
+    index: "./src/index.js"
   },
   output:{
-    path:path.resolve(__dirname,"dist"),
-    filename:'[name].js'
+    path    : path.resolve(__dirname,"dist"),
+    filename: '[name].js'
   },
   module:{
     rules:[
-    {
-      test:/\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: "style-loader",
-        use: "css-loader",
-        publicPath:"../"
-      })
-    },{
-      test:/\.(jpg|png|gif)$/,
-      use:[{
-        loader:'url-loader',
-        options:{
-          limit:300,
-          outputPath:"images/"
+      {
+        test: /\.css$/,
+        use : ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use     : [{
+            loader : "css-loader",
+            options: {importLoaders:1}
+        },"postcss-loader"],
+          publicPath: "../"
+        })
+      },{
+        test: /\.(jpg|png|gif)$/,
+        use : [{
+          loader : 'url-loader',
+          options: {
+            limit     : 300,
+            outputPath: "image/"
+          }
+        }]
+      },{
+        test  : /\.html$/,
+        loader: 'html-withimg-loader'
+      },{
+        test: /\.scss/,
+        use : ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use     : ["css-loader","sass-loader"]
+        })
+      },{
+        test   : /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use    : {
+        loader : 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
         }
-      }]
-    },{
-      test:/\.html$/,
-      loader:'html-withimg-loader'
-    }
-  ]
+      }
+      }
+    ]
   },
   plugins:[
     new ExtractTextPlugin("css/main.css"),
+    new PurifyCSSPlugin({
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
+    }),
     new HtmlWebpackPlugin({  
       minify:{
-        removeAttributeQuotes:true
+        removeAttributeQuotes: true
       },
-      hash:true,
+      hash    : true,
       filename: 'index.html',
       template: 'src/index.html'
     })
   ],
   devServer:{
-    contentBase:path.resolve(__dirname,"dist"),
-    host:"localhost",
-    compress:true,
-    port:"8081"
+    contentBase: path.resolve(__dirname,"dist"),
+    host       : "localhost",
+    compress   : true,
+    port       : "8081"
   }
 }
