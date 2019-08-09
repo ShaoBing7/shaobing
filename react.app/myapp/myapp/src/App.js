3a5d8ea7505f7f49b565a65e72341fce45f6cda7
ShaoBing7 <1556594881@qq.com>
2019-08-09 11:06:05 +0800

    4

--- react.app/myapp/myapp/src/App.js
+++ react.app/myapp/myapp/src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Route,  
-  Link,}from "react-router-dom";
+  Link,
+  Switch
+}from "react-router-dom";
+import Test1 from "./component/Test1"
+import Test2 from "./component/Test2"
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+       <Link to="/">test1</Link>
+       <Link to="/test2">test2</Link>
+       <Switch>
+         <Route path="/test2" component={Test2}/>
+         <Route path="/" component={Test1}/>
+       </Switch>
+     </Router>
     </div>
   );
 }
--- react.app/myapp/myapp/src/component/test2.js
+++ react.app/myapp/myapp/src/component/test2.js
@@ -1,1 +1,12 @@
-import React,{Component}
+import React, { Component } from 'react'
+export default class Test2 extends Component{
+    render(){
+        return(
+            <div>
+                <h1>
+                    test2
+                </h1>
+            </div>
+        )
+    }
+}
